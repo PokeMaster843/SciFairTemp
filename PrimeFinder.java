fec10a1d4bceb4cbab693173348bda848ec839ed
John Knickerbocker <PokeMaster843@gmail.com>
2016-10-17 07:26:55 -0400

    Update PrimeFinder.java

--- PrimeFinder.java
+++ PrimeFinder.java
@@ -2,6 +2,15 @@ package com.thepokemaster843.primefinder;
 
 class PrimeFinder {
   
-  
+  public static void main(String[] args) {
+    
+    int currentNum;
+    boolean isPrime = false;
+    
+    while() {
+      
+    }
+    
+  }
   
 }
