df178783f7f70ccd6553802cac0d43c1fad45090
John Knickerbocker <PokeMaster843@gmail.com>
2016-10-31 07:21:47 -0400

    Update Blowfish.java

--- Blowfish.java
+++ Blowfish.java
@@ -1,1 +1,25 @@
+package com.thepokemaster843.scifairencryption;
 
+public class Blowfish {
+  
+  public long F(long x) {
+    
+    short a, b, c, d, y;
+    d = x & 0x00ff;
+    x >>= 8;
+    c = x & 0x00ff;
+    x >>= 8;
+    b = x & 0x00ff;
+    x >>= 8;
+    a = x & 0x00ff;
+    
+    y = sBox[0][a];
+    y += sBox[1][b];
+    y ^= sBox[2][c];
+    y += s[3][d];
+    
+    return y;
+    
+  }
+    
+}
